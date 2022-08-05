a2e4b26a2684f9f6df2973a1999e2ca1520602ec
cmong0516 <cmong0516@gmail.com>
2022-08-05 16:50:01 +0900

    2022.08.05 오타를 조심하자

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -3,14 +3,18 @@ package hello.exception;
 
 import hello.exception.filter.LogFilter;
 import hello.exception.interceptor.LogInterceptor;
+import hello.exception.resolver.MyHandlerExceptionResolver;
+import hello.exception.resolver.UserExceptionHandlerResolver;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -38,5 +42,9 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**");
     }
 
-
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+        resolvers.add(new UserExceptionHandlerResolver());
+    }
 }
--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -7,16 +7,30 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 
-@Component
-public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+//@Component
+//public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+//    @Override
+//    public void customize(ConfigurableWebServerFactory factory) {
+//
+//        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error- page/404");
+//        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+//        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error- page/500");
+//
+//        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+//
+//    }
+//}
+
+
+//@Component
+public class WebServerCustomizer implements
+        WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
-
-        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error- page/404");
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
-        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error- page/500");
-
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+        // 띄어쓰기 주의하자 ........
         factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
-
     }
 }
